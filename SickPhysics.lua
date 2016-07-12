8dd9911a291945a99339693bf4937fc08ff9bfa6
SickBow <mikecilona@gmail.com>
2016-07-12 08:50:57 -0400

    Update SickPhysics.lua
    starting to add functionality. X axis movement.

--- SickPhysics.lua
+++ SickPhysics.lua
@@ -1,7 +1,7 @@
 physics = {}
 physics.world = {}
 
---physics objects can be referenced and manipulated (adding force, subtracting force functions) with "physics.world.(object name here).(specific function name here)"
+--physics objects can be referenced and manipulated (adding force, subtracting force) with "physics.world.(object name here).(specific function name here)"
 --this only works after adding them to the world/simulation due to them having unique names the user may create
 --each object will be initialized with its own gravity, in case you want to make a bird or a static object (floors or surfaces)
 
@@ -13,14 +13,21 @@ function physics:newRectangle ( startX, startY, width, height, gravity )
 	self.X, self.Y = startX, startY
 	self.width, self.height = width, height
 	self.gravity = gravity
+	self.grounded = false
 	self.forceX, self.forceY = 0,0
 	
 	self.addForce = function (forceX, forceY )
 		self.forceX, self.forceY = forceX, forceY
 	end
 	
-	self.update = function (deltaTime)
+	self.update = function (dt)
+		if self.forceX < 0 then
+			self.forceX = self.forceX + 10*(dt)
+		elseif self.forceX > 0 then
+			self.forceX = self.forceX - 10*(dt)
+		end
 		
+		self.X = self.X + self.forceX 
 	end
 	
 	return self
